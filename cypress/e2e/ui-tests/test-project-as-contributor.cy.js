1702be188665b753b68ed03877292f948f5c5e30
akochari <arnold.kochari@scilifelab.se>
2023-09-14 16:51:06 +0200

    adjust tests broken by fixed navbar

--- cypress/e2e/ui-tests/test-project-as-contributor.cy.js
+++ cypress/e2e/ui-tests/test-project-as-contributor.cy.js
@@ -30,7 +30,7 @@ describe("Test project contributor user functionality", () => {
     it("can run the test setup", () => {
     })
 
-    it("can create a new blank project", () => {
+    it("can create a new blank project", { scrollBehavior: false }, () => { // need to disable scrolling due to our fixed navbar
 
         // Names of objects to create
         const project_name = "e2e-create-proj-test"
