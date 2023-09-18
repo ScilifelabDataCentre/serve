13998698ce227d9dfeffe8d664b4abbb989c9b94
Nikita Churikov <nikita@chur.ru>
2023-09-18 16:36:36 +0200

    WIP working on sign up form customisation

--- common/forms.py
+++ common/forms.py
@@ -1,16 +1,56 @@
+from typing import Optional, Sequence
+import re
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.validators import EmailValidator
+from django.utils.safestring import mark_safe
+from django.core.exceptions import ValidationError
+
+
+EMAIL_ALLOW_REGEX = re.compile(
+        (r"^(?:.*\.)?("
+         "uu"            # Uppsala universitets webbplats"
+         "|lu"           # Lunds universitets webbplats"
+         "|gu"           # Göteborgs universitets webbplats"
+         "|su"           # Stockholms universitets webbplats"
+         "|umu"          # Umeå universitets webbplats"
+         "|liu"          # Linköpings universitets webbplats"
+         "|ki"           # Karolinska institutets webbplats"
+         "|kth"          # Kungliga Tekniska högskolans webbplats"
+         "|chalmers"     # Chalmers tekniska högskolas webbplats"
+         "|ltu"          # Luleå tekniska universitets webbplats"
+         "|hhs"          # Handelshögskolan i Stockholms webbplats"
+         "|slu"          # Sveriges lantbruksuniversitets webbplats"
+         "|kau"          # Karlstads universitets webbplats"
+         "|lnu"          # Linnéuniversitetets webbplats"
+         "|oru"          # Örebro universitets webbplats"
+         "|miun"         # Mittuniversitetets webbplats"
+         "|mau"          # Malmö universitets webbplats"
+         "|mdu"          # Mälardalens universitets webbplats"
+         "|bth"          # Blekinge tekniska högskolas webbplats"
+         "|fhs"          # Försvarshögskolans webbplats"
+         "|gih"          # Gymnastik- och idrottshögskolans webbplats"
+         "|hb"           # Högskolan i Borås webbplats"
+         "|du"           # Högskolan Dalarnas webbplats"
+         "|hig"          # Högskolan i Gävles webbplats"
+         "|hh"           # Högskolan i Halmstads webbplats"
+         "|hkr"          # Högskolan Kristianstads webbplats"
+         "|his"          # Högskolan i Skövdes webbplats"
+         "|hv"           # Högskolan Västs webbplats"
+         "|ju"           # Stiftelsen Högskolan i Jönköpings webbplats"
+         "|sh"           # Södertörns högskolas webbplats"
+         "|nbis"         # National Bioinformatics Infrastructure Sweden
+         "|scilifelab"   # Science for Life Laboratory
+         ")\.se$"
+        )
+)
 
 
 # Sign Up Form
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(
-        max_length=30,
-        required=True,
-        label=False,
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username*"}),
-    )
+
     first_name = forms.CharField(
         max_length=30,
         required=False,
@@ -27,6 +67,7 @@ class SignUpForm(UserCreationForm):
         max_length=254,
         label=False,
         widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Email*"}),
+        validators=(EmailValidator(allowlist=["uu.se"]),),
     )
     password1 = forms.CharField(
         label=False,
@@ -47,3 +88,15 @@ class SignUpForm(UserCreationForm):
             "password1",
             "password2",
         ]
+
+    def clean_email(self) -> str:
+        email = self.cleaned_data["email"]
+        if EMAIL_ALLOW_REGEX.match(email) is None:
+            raise ValidationError(
+                    mark_safe(
+                        "Please use your <a "
+                        "href='https://www.uka.se/sa-fungerar-hogskolan/universitet-och-hogskolor/lista-over-"
+                        "universitet-hogskolor-och-enskilda-utbildningsanordnare'>"
+                        "swedish university</a> email address."
+                        )
+                    )
--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,42 @@
+import pytest
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django import forms
+from hypothesis import given, settings, Verbosity, strategies as st
+from hypothesis.extra.django import from_field
 
-# Create your tests here.
+from common.forms import SignUpForm, EMAIL_ALLOW_REGEX
+
+@given(email=from_field(forms.EmailField()))
+def test_raise_not_swedish_university_email_validation(email):
+    form = SignUpForm(
+        {
+            "username": "test",
+            "first_name": "test",
+            "last_name": "test",
+            "email": "noname@xyz.se",
+            "password1": "test",
+            "password2": "test",
+        }
+    )
+    form.cleaned_data = {"email": email}
+    form.clean("email")
+    with pytest.raises(ValidationError):
+        form.clean_email()
+
+
+@given(email=st.from_regex(EMAIL_ALLOW_REGEX))
+def test_pass_swedish_university_email_validation(email):
+    form = SignUpForm(
+        {
+            "username": "test",
+            "first_name": "test",
+            "last_name": "test",
+            "email": email,
+            "password1": "test",
+            "password2": "test",
+        }
+    )
+    form.cleaned_data = {"email": email}
+    form.clean_email()
+
