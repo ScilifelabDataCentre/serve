ea4cc7d6bb316dcd23e5f1b30c5caaccaa0b4de5
Arnold Kochari <arnold.kochari@scilifelab.se>
2023-09-20 17:22:47 +0200

    project and app number limitations (#65)

--- cypress/e2e/ui-tests/test-project-as-contributor.cy.js
+++ cypress/e2e/ui-tests/test-project-as-contributor.cy.js
@@ -30,7 +30,7 @@ describe("Test project contributor user functionality", () => {
     it("can run the test setup", () => {
     })
 
-    it("can create a new blank project", () => {
+    it("can create a new blank project, open settings, delete from settings", () => {
 
         // Names of objects to create
         const project_name = "e2e-create-proj-test"
@@ -60,8 +60,6 @@ describe("Test project contributor user functionality", () => {
                 cy.get("title").should("have.text", project_title_name)
                 cy.get('h3').should('contain', project_name)
 
-                // TODO: add tests for all sections similar to Models below
-
                 // Section Models - Machine Learning Models
                 // Navigate to the create models view and cancel back again
                 cy.get("div#models").first("h5").should("contain", "Machine Learning Models")
@@ -79,13 +77,26 @@ describe("Test project contributor user functionality", () => {
                 cy.get('[data-cy="settings"]').click()
                 cy.url().should("include", "settings")
                 cy.get('h3').should('contain', 'Project settings')
+
+                // Delete the project from the settings menu
+                cy.get('a').contains("Delete").click()
+                .then((href) => {
+                    cy.get('div#delete').should('have.css', 'display', 'block')
+                    cy.get('#id_delete_button').parent().parent().find('button').contains('Delete').click()
+                    .then((href) => {
+                        cy.get('div#deleteModal').should('have.css', 'display', 'block')
+                        cy.get('div#deleteModal').find('button').contains('Confirm').click()
+                    })
+                    cy.contains(project_name).should('not.exist')
+
+                   })
             })
     })
 
     it.skip("can create a new mlflow project", () => {
     })
 
-    it("can delete a project", () => {
+    it("can delete a project from projects overview", () => {
 
         // Names of objects to create
         const project_name = "e2e-delete-proj-test"
@@ -111,5 +122,72 @@ describe("Test project contributor user functionality", () => {
                     cy.contains(project_name).should('not.exist')
                })
             })
+    })
+
+    it("limit on number of apps per project is enforced", () => {
+        // Names of objects to create
+        const project_name = "e2e-create-proj-test"
+
+        // Create a project
+        cy.visit("/projects/")
+        cy.get("a").contains('New project').click()
+        cy.get("a").contains('Create').first().click()
+        cy.get('input[name=name]').type(project_name)
+        cy.get("input[name=save]").contains('Create project').click()
+            .then((href) => {
+                cy.log(href)
+                // Check that the app limits work using Jupyter Lab as example
+                // step 1. create persistent volume
+                cy.get('[data-cy="create-app-card"]').contains('Persistent Volume').parent().siblings().find('.btn').click()
+                cy.get('input[name=app_name]').type("e2e-create-pv")
+                cy.get('.btn-primary').contains('Create').click()
+                // step 2. create 3 jupyter lab instances (current limit)
+                Cypress._.times(3, () => {
+                        cy.get('[data-cy="create-app-card"]').contains('Jupyter Lab').parent().siblings().find('.btn').click()
+                        cy.get('input[name=app_name]').type("e2e-create-jl")
+                        cy.get('.btn-primary').contains('Create').click()
+                  });
+                // step 3. check that the button to create another one does not work
+                cy.get('[data-cy="create-app-card"]').contains('Jupyter Lab').parent().siblings().find('.btn').should('not.have.attr', 'href')
+                // step 4. check that it is not possible to create another one using direct url
+                let projectURL
+                    cy.url().then(url => {
+                        projectURL = url
+                    });
+                cy.then(() =>
+                    cy.request({url: projectURL + "/apps/create/jupyter-lab?from=overview", failOnStatusCode: false}).its('status').should('equal', 403)
+                    )
+                })
+
+        // Delete the created project
+        cy.visit("/projects/")
+        cy.get('h5.card-title').contains(project_name).siblings('div').find('a.confirm-delete').click()
+            .then((href) => {
+                cy.get("h1#modalConfirmDeleteLabel").then(function($elem) {
+                    cy.get('div#modalConfirmDeleteFooter').find('button').contains('Delete').click()
+               })
+            })
+    })
+
+    it("limit on number of projects per user is enforced", () => {
+        // Names of projects to create
+        const project_name = "e2e-create-proj-test"
+
+        // Create 5 projects (current limit)
+        Cypress._.times(5, () => {
+            cy.visit("/projects/")
+            cy.get("a").contains('New project').click()
+            cy.get("a").contains('Create').first().click()
+            cy.get('input[name=name]').type(project_name)
+            cy.get("input[name=save]").contains('Create project').click()
+        });
+
+        // Now check that it is not possible to create another project
+        // not possible to click the button to create a new project
+        cy.visit("/projects/")
+        cy.get("button").contains('New project').should('not.have.attr', 'href')
+        // if accessing directly with the url, the request is not accepted
+        cy.request({url: "/projects/create", failOnStatusCode: false}).its('status').should('equal', 403)
+        cy.request({url: "/projects/templates", failOnStatusCode: false}).its('status').should('equal', 403)
     })
 })
