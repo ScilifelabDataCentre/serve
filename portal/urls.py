b67e003591ac7eea85fee261dd3aa7adfdb357be
akochari <arnold.kochari@scilifelab.se>
2023-10-05 08:29:33 +0200

    logged in users redirected to projects when calling /

--- portal/urls.py
+++ portal/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
+from projects.views import IndexView as IndexView
+
 from . import views
 
 app_name = "portal"
 
 urlpatterns = [
-    path("portal/home", views.HomeView.as_view(), name="home"),
+    path("home", views.HomeView.as_view(), name="home"),
     path("about", views.about, name="about"),
     path("teaching", views.teaching, name="teaching"),
     path("privacy", views.privacy, name="privacy"),
-    path("apps", views.public_apps, name="apps"),
-    path("", views.HomeView.as_view(), name="about-page"),
+    path("apps/", views.public_apps, name="apps"),
+    path("", views.HomeViewDynamic.as_view(), name="home-dynamic"),
 ]
