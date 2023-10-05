97ed33b0185028aede6d4df4f01a0821c0a55883
akochari <arnold.kochari@scilifelab.se>
2023-10-05 08:41:18 +0200

    better urls to make use of django appending slashes

--- portal/urls.py
+++ portal/urls.py
@@ -7,10 +7,10 @@ from . import views
 app_name = "portal"
 
 urlpatterns = [
-    path("home", views.HomeView.as_view(), name="home"),
-    path("about", views.about, name="about"),
-    path("teaching", views.teaching, name="teaching"),
-    path("privacy", views.privacy, name="privacy"),
+    path("home/", views.HomeView.as_view(), name="home"),
+    path("about/", views.about, name="about"),
+    path("teaching/", views.teaching, name="teaching"),
+    path("privacy/", views.privacy, name="privacy"),
     path("apps/", views.public_apps, name="apps"),
     path("", views.HomeViewDynamic.as_view(), name="home-dynamic"),
 ]
