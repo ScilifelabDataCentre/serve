22f85a0113a37516afed433a69cacbf9983b180a
akochari <arnold.kochari@scilifelab.se>
2023-10-18 17:46:52 +0200

    special user for the reader test

---
+++ cypress/e2e/setup-scripts/seed_reader_user.py
@@ -0,0 +1,25 @@
+"""DB seed script for e2e cypress login tests."""
+
+import json
+import os.path
+
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from projects.models import Project
+
+cypress_path = os.path.join(settings.BASE_DIR, "cypress/fixtures")
+print(f"Now loading the json users file from fixtures path: {cypress_path}")  # /app/cypress/fixtures
+
+with open(os.path.join(cypress_path, "users.json"), "r") as f:
+    testdata = json.load(f)
+
+    userdata = testdata["reader_user"]
+
+    username = userdata["username"]
+    email = userdata["email"]
+    pwd = userdata["password"]
+
+    # Create the reader user
+    user = User.objects.create_user(username, email, pwd)
+    user.save()
