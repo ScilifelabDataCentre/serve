b33be790d56cc3e32eb66e31aa61561be64608fe
sandstromviktor <v.sandstrom@icloud.com>
2023-10-27 09:40:03 +0200

    fixed variable name error

--- cypress/e2e/ui-tests/test-signup.cy.js
+++ cypress/e2e/ui-tests/test-signup.cy.js
@@ -1,6 +1,6 @@
 describe("Test sign up", () => {
 
-    let userdata
+    let users
 
     beforeEach(() => {
         // reset and seed the database prior to every test
@@ -15,9 +15,8 @@ describe("Test sign up", () => {
     })
 
     beforeEach(() => {
-        // username in fixture must match username in db-reset.sh
         cy.fixture('users.json').then(function (data) {
-            users = data.signup_user;
+            users = data;
           })
     })
 
@@ -26,11 +25,11 @@ describe("Test sign up", () => {
         cy.visit("/signup/");
         cy.get("title").should("have.text", "Register | SciLifeLab Serve")
 
-        cy.get('input[name=email]').type(users.email);
+        cy.get('input[name=email]').type(users.signup_user.email);
         cy.get('input[name=first_name]').type("first name");
         cy.get('input[name=last_name]').type("last name");
-        cy.get('input[name=password1]').type(users.password);
-        cy.get('input[name=password2]').type(users.password);
+        cy.get('input[name=password1]').type(users.signup_user.password);
+        cy.get('input[name=password2]').type(users.signup_user.password);
         cy.get('input[name="department"]').type('Biology Education Centre');
 
         cy.get("input#submit-id-save").click();
