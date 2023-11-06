3fc3473ce708cf5f6837c502187e671b4737c3b7
Viktor Sandström <v.sandstrom@icloud.com>
2023-11-06 13:46:04 +0100

    Bugfix/SS 637 adding tags with spaces for apps not working properly (#95)
    * Fixed tag logic. Can create multiple tags at once
    * Styling consistent across the UI
    * Fixed dev superuser issue

--- scripts/admin_token.py
+++ scripts/admin_token.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-
+import os
 User = get_user_model()
 
-
 def run(*args):
-    admin = User.objects.get(username="admin")
+    if not User.objects.filter(email='admin@test.com').exists():
+        User.objects.create_superuser('admin', 'admin@test.com', os.getenv("DJANGO_SUPERUSER_PASSWORD"))
+        
+    admin = User.objects.get(username="admin@test.com")
     try:
         _ = Token.objects.get(user=admin)
     except Token.DoesNotExist:
