a717d921f0b3212451bf9478ee787e8954bb038f
alfredeen <johan.alfredeen@gmail.com>
2023-11-08 08:52:09 +0100

    Reformatting.

--- scripts/admin_token.py
+++ scripts/admin_token.py
@@ -1,12 +1,15 @@
+import os
+
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-import os
+
 User = get_user_model()
 
+
 def run(*args):
-    if not User.objects.filter(email='admin@test.com').exists():
-        User.objects.create_superuser('admin', 'admin@test.com', os.getenv("DJANGO_SUPERUSER_PASSWORD"))
-        
+    if not User.objects.filter(email="admin@test.com").exists():
+        User.objects.create_superuser("admin", "admin@test.com", os.getenv("DJANGO_SUPERUSER_PASSWORD"))
+
     admin = User.objects.get(username="admin@test.com")
     try:
         _ = Token.objects.get(user=admin)
