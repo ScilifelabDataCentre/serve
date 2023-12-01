c30f11b371eb0fa1c1a3c05c26fa3324fe218c22
Nikita Churikov <nikita@chur.ru>
2023-12-01 17:49:55 +0100

    added comments to common tests on how they work

--- common/tests.py
+++ common/tests.py
@@ -70,47 +70,82 @@ def input_form(
     return form
 
 
+# Override the INACTIVE_USERS setting for this test case
+# This results in users being inactive after registration
 @override_settings(INACTIVE_USERS=True)
 class TestSignUp(TransactionTestCase):
+    # Using hypothesis for generating test cases with input_form
     @given(form=input_form())
-    @settings(verbosity=Verbosity.verbose, max_examples=1, deadline=None)
+    @settings(verbosity=Verbosity.verbose, max_examples=1, deadline=None)  # Configure hypothesis settings
     def test_pass_validation(self, form):
-        UserProfile.objects.all().delete()
-        User.objects.all().delete()
+        # Check if the form is valid
+        # is_val call implicitly calls the form's clean methods, which populates the cleaned_data attribute
         is_val = form.is_valid()
+
+        # Assert that the user and profile forms have cleaned_data attribute.
+        # It means that the clean methods were called
         assert hasattr(form.user, "cleaned_data")
         assert hasattr(form.profile, "cleaned_data")
+
+        # Assert that the form is valid; if not, show form errors
         assert is_val, (form.user.errors, form.profile.errors)
+
+        # Save the form and obtain the profile instance
         profile = form.save()
+
+        # Check if the username is the same as the email, which is a common pattern
         assert profile.user.username == profile.user.email
-        # Verify email sending
+
+        # Verify that an email has been sent
         assert mail.outbox[0].subject == "Verify your email address on SciLifeLab Serve"
-        assert mail.outbox[0].to == [profile.user.email]
+        assert mail.outbox[0].to == [profile.user.email]  # Check if the email is sent to the user's email
+
+        # Check if an entry in EmailVerificationTable exists for the user
         assert EmailVerificationTable.objects.filter(user=profile.user).exists()
+
+        # Retrieve the token from EmailVerificationTable and check if it's in the email body
         token = EmailVerificationTable.objects.get(user=profile.user).token
         assert token in mail.outbox[0].body
 
 
 @override_settings(INACTIVE_USERS=True)
 class TestEmailSending(TransactionTestCase):
+    # Use hypothesis for property-based testing
     @given(form=input_form())
-    @settings(verbosity=Verbosity.verbose, max_examples=1, deadline=None)
+    @settings(verbosity=Verbosity.verbose, max_examples=1, deadline=None)  # Set Hypothesis test settings
     def test_email_verification_email_not_from_university(self, form):
-        UserProfile.objects.all().delete()
-        User.objects.all().delete()
+        # Check if the form is valid
+        # is_val call implicitly calls the form's clean methods, which populates the cleaned_data attribute
         is_val = form.is_valid()
+
+        # Assert that the user and profile forms have cleaned_data attribute.
+        # It means that the clean methods were called
         assert hasattr(form.user, "cleaned_data")
         assert hasattr(form.profile, "cleaned_data")
+
+        # Assert form is valid; if not, show form errors
         assert is_val, (form.user.errors, form.profile.errors)
+
+        # Explicitly set form approval to False
+        # This is required, becuase emails that are being generated are from university,
+        # which sets the form to approved
+        # But we need to test the case when the email is not from university,
+        # which is essentially when is_approved is False
         form.is_approved = False
-        form.save()
+        form.save()  # Save the form changes
+
+        # Retrieve the user object based on the email from the form's cleaned data
         user = User.objects.get(email=form.user.cleaned_data["email"])
 
+        # Retrieve the token for the user from EmailVerificationTable
         token = EmailVerificationTable.objects.get(user=user.id).token
-        assert token in mail.outbox[0].body
+        assert token in mail.outbox[0].body  # Check if the token is in the first sent email
 
+        # Simulate a POST request to the verification URL with the token
         resp = self.client.post("/verify/", {"token": token})
-        assert resp.status_code == 302
+        assert resp.status_code == 302  # Check if the response is a redirect (status code 302)
+
+        # Assert that the second email sent contains the specific activation message
         assert f"Please go to the admin page to activate account for {user.email}" in mail.outbox[1].body
 
 
