33f6bbfae13f8ba4832904e362ab139ced781fb1
Hamza Imran Saeed <hamza.saeed@scilifelab.uu.se>
2023-12-08 21:43:14 +0100

    fix linting

--- common/models.py
+++ common/models.py
@@ -4,7 +4,9 @@ from django.core.mail import send_mail
 from django.db import models
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+
 from studio.settings import DOMAIN
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
