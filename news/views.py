b0bfca3097dc1e0a295e185aff02d53fc97eab2f
akochari <arnold.kochari@scilifelab.se>
2023-12-21 05:46:12 +0100

    explicitly passing news_objects

--- news/views.py
+++ news/views.py
@@ -8,4 +8,4 @@ def news(request):
     news_objects = NewsObject.objects.all().order_by("-created_on")
     for news in news_objects:
         news.body_html = markdown.markdown(news.body)
-    return render(request, "news/news.html", locals())
+    return render(request, "news/news.html", {"news_objects": news_objects})
