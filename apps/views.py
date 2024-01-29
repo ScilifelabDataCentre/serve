07c549530a0be55eacf882f118d82296bfe01aa3
Viktor Sandström <v.sandstrom@icloud.com>
2024-01-29 14:12:16 +0100

    Update apps/views.py
    Co-authored-by: Nikita Churikov <8545082+churnikov@users.noreply.github.com>

--- apps/views.py
+++ apps/views.py
@@ -275,10 +275,7 @@ class AppSettingsView(View):
     def update_resource(self, request, appinstance, current_release_name):
         domain = appinstance.parameters["global"]["domain"]
         # if subdomain is set as --generated--, then use appname
-        if request.POST.get("app_release_name"):
-            new_release_name = request.POST.get("app_release_name")
-        else:
-            new_release_name = appinstance.parameters["appname"]
+        new_release_name =  request.POST.get("app_release_name", appinstance.parameters["appname"])
 
         new_url = f"https://{new_release_name}.{domain}"
         appinstance.table_field.update({"url": new_url})
