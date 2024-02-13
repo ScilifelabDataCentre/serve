54979eecd42affa204b540643e15e9b4b55d9d95
sandstromviktor <v.sandstrom@icloud.com>
2024-02-13 14:58:05 +0100

    removed superfluous noqa comment

--- studio/celery.py
+++ studio/celery.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
-from logging.config import dictConfig  # noqa
+from logging.config import dictConfig
 
 from celery import Celery
 from celery.signals import setup_logging
