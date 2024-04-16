1d04a6f0bd6fede5780af0c3f6ad54f0a19eea12
akochari <arnold.kochari@scilifelab.se>
2024-04-16 12:59:10 +0200

    change is disabled login variable name

--- customtags/templatetags/is_login_signup_disabled.py
+++ customtags/templatetags/is_login_signup_disabled.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name="is_disabled")
-def is_disabled(queryset):
+@register.filter(name="is_login_signup_disabled")
+def is_login_signup_disabled(queryset):
     return queryset.filter(login_and_signup_disabled=True).exists()
