e3518532319288942412f53a264c28682c58eb66
alfredeen <johan.alfredeen@gmail.com>
2024-05-07 17:10:52 +0200

    Adding type hints to the studio module.

--- studio/singleton.py
+++ studio/singleton.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Any, Dict
 
 
 class Singleton(type):
@@ -9,7 +9,7 @@ class Singleton(type):
 
     _instances: Dict[type, object] = {}
 
-    def __call__(cls, *args, **kwargs):
+    def __call__(cls: Any, *args, **kwargs) -> Any:  # type: ignore[no-untyped-def]
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
