ea7b7c17c2cf57210cfe1df6d806f70a87e19f65
sandstromviktor <v.sandstrom@icloud.com>
2024-05-28 12:04:16 +0200

    test forms

---
+++ apps/tests/test_forms.py
@@ -0,0 +1,145 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from apps.forms import CustomAppForm
+from apps.models import Apps, AppStatus, Subdomain, VolumeInstance
+from projects.models import Flavor, Project
+
+User = get_user_model()
+
+test_user = {"username": "foo1", "email": "foo@test.com", "password": "bar"}
+
+
+class BaseAppFormTest(TestCase):
+    def setUp(self):
+        # Create instances of RelatedModel
+        self.user = User.objects.create_user(test_user["username"], test_user["email"], test_user["password"])
+        self.project = Project.objects.create_project(name="test-perm", owner=self.user, description="")
+        self.app = Apps.objects.create(name="Serve App", slug="serve-app")
+        self.volume = VolumeInstance.objects.create(
+            name="project-vol",
+            app=self.app,
+            owner=self.user,
+            project=self.project,
+            size=1,
+            subdomain=Subdomain.objects.create(subdomain="subdomain"),
+            app_status=AppStatus.objects.create(status="Created"),
+        )
+        self.flavor = Flavor.objects.create(name="flavor", project=self.project)
+
+
+class CustomAppFormTest(BaseAppFormTest):
+    def setUp(self):
+        super().setUp()
+        self.valid_data = {
+            "name": "Valid Name",
+            "description": "A valid description",
+            "subdomain": "valid-subdomain",
+            "volume": self.volume,
+            "path": "/home/user",
+            "flavor": self.flavor,
+            "access": "public",
+            "source_code_url": "http://example.com",
+            "note_on_linkonly_privacy": None,
+            "port": 8000,
+            "image": "ghcr.io/scilifelabdatacentre/image:tag",
+            "tags": ["tag1", "tag2", "tag3"],
+        }
+
+    def test_form_valid_data(self):
+        form = CustomAppForm(self.valid_data, project_pk=self.project.pk)
+        self.assertTrue(form.is_valid())
+
+    def test_form_missing_data(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data.pop("name")
+        invalid_data.pop("port")
+        invalid_data.pop("image")
+
+        form = CustomAppForm(invalid_data, project_pk=self.project.pk)
+        self.assertFalse(form.is_valid())
+        self.assertIn("name", form.errors)
+        self.assertIn("port", form.errors)
+        self.assertIn("image", form.errors)
+
+    def test_invalid_path(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data["path"] = "/var"
+
+        form = CustomAppForm(invalid_data, project_pk=self.project.pk)
+        self.assertFalse(form.is_valid())
+        self.assertIn("Path must start with", str(form.errors))
+
+    def test_valid_path_if_set_in_admin_panel(self):
+        valid_data = self.valid_data.copy()
+        form = CustomAppForm(valid_data, project_pk=self.project.pk)
+        self.assertTrue(form.is_valid())
+
+        # Simulate a path set in the admin panel
+        instance = form.save(commit=False)
+        instance.project = self.project
+        instance.owner = self.user
+        instance.app_status = AppStatus.objects.create(status="Created")
+        instance.app = self.app
+
+        # Fetch subdomain and set
+        subdomain_name = form.cleaned_data.get("subdomain")
+        subdomain, _ = Subdomain.objects.get_or_create(subdomain=subdomain_name, project=self.project)
+        instance.subdomain = subdomain
+
+        # Change the path to something that the form would not allow
+        instance.path = "/var"
+        instance.save()
+
+        # Open form in "edit mode" by sending instance as argument
+        form = CustomAppForm(valid_data, project_pk=self.project.pk, instance=instance)
+
+        # This should now work, since we set the path in the "admin panel"
+        self.assertTrue(form.is_valid())
+
+    def test_volume_no_path(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data.pop("path")
+
+        form = CustomAppForm(invalid_data, project_pk=self.project.pk)
+        self.assertFalse(form.is_valid())
+        self.assertIn("Path is required when volume is selected.", str(form.errors))
+
+    def test_path_no_volume(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data.pop("volume")
+
+        form = CustomAppForm(invalid_data, project_pk=self.project.pk)
+        self.assertFalse(form.is_valid())
+        self.assertIn("Warning, you have provided a path, but not selected a volume.", str(form.errors))
+
+    def test_invalid_subdomain(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data["subdomain"] = "-some_invalid_subdomain!"
+
+        form = CustomAppForm(invalid_data, project_pk=self.project.pk)
+        self.assertFalse(form.is_valid())
+        self.assertIn("Subdomain must be 3-53 characters long", str(form.errors))
+
+    def test_source_url_enforced_when_public(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data["source_code_url"] = ""
+
+        form = CustomAppForm(invalid_data, project_pk=self.project.pk)
+        self.assertFalse(form.is_valid())
+
+    def test_link_only_note_enforced_when_link(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data["access"] = "link"
+
+        # Test no note
+        form = CustomAppForm(invalid_data, project_pk=self.project.pk)
+        self.assertFalse(form.is_valid())
+        self.assertIn("Please, provide a reason for making the app accessible only via a link.", str(form.errors))
+
+        # Now add a note
+        valid_data = self.valid_data.copy()
+        valid_data["access"] = "link"
+        valid_data["note_on_linkonly_privacy"] = "A reason"
+        form = CustomAppForm(valid_data, project_pk=self.project.pk)
+        self.assertTrue(form.is_valid())
