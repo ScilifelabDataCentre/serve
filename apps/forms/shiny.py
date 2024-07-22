9e4e5ce29f1ede51b248d5cf5737f5ed9b9573ae
Hamza <hamzaimran08@gmail.com>
2024-07-22 20:37:31 +0200

    remove proxy option from form

--- apps/forms/shiny.py
+++ apps/forms/shiny.py
@@ -12,21 +12,13 @@ class ShinyForm(AppBaseForm):
     flavor = forms.ModelChoiceField(queryset=Flavor.objects.none(), required=False, empty_label=None)
     port = forms.IntegerField(min_value=3000, max_value=9999, required=True)
     image = forms.CharField(max_length=255, required=True)
-    proxy = forms.BooleanField(
-        required=False,
-        initial=False,
-        label="Activate Proxy",
-        help_text="""<i class='bi bi-question-circle me-2'></i> Check this box if your
-            app requires lots of compute power. This will enable a proxy to handle the load.
-            """,
-    )
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         if self.instance and self.instance.pk:
             self.initial_subdomain = self.instance.subdomain.subdomain
-            self.initial_proxy = self.instance.proxy
 
     def _setup_form_fields(self):
         # Handle Volume field
@@ -37,9 +29,6 @@ class ShinyForm(AppBaseForm):
         body = Div(
             self.get_common_field("name", placeholder="Name your app"),
             self.get_common_field("description", rows="3", placeholder="Provide a detailed description of your app"),
-            Field(
-                "proxy",
-            ),
             self.get_common_field(
                 "subdomain", placeholder="Enter a subdomain or leave blank for a random one", spinner=True
             ),
@@ -66,15 +55,7 @@ class ShinyForm(AppBaseForm):
         if access == "public" and not source_code_url:
             self.add_error("source_code_url", "Source is required when access is public.")
 
-        proxy = cleaned_data.get("proxy")
         subdomain = cleaned_data.get("subdomain")
-
-        # Check if boolfield has changed
-        if self.instance and self.instance.pk:
-            if proxy != self.initial_proxy:
-                # Ensure charfield is also changed
-                if subdomain == self.initial_subdomain:
-                    self.add_error("subdomain", "Subdomain must be changed if proxy is changed.")
 
         return cleaned_data
 
@@ -83,7 +64,6 @@ class ShinyForm(AppBaseForm):
         fields = [
             "name",
             "description",
-            "proxy",
             "volume",
             "flavor",
             "access",
