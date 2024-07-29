0539b3c518bdc8d7a15f7742e2fe8c466abec692
Nikita Churikov <nikita@chur.ru>
2024-07-29 16:06:23 +0200

    Change default app permission level to `project` in all apps

--- apps/models/app_types/tissuumaps.py
+++ apps/models/app_types/tissuumaps.py
@@ -26,7 +26,7 @@ class TissuumapsInstance(BaseAppInstance, SocialMixin, LogsEnabledMixin):
     volume = models.ForeignKey(
         "VolumeInstance", blank=True, null=True, related_name="%(class)s", on_delete=models.CASCADE
     )
-    access = models.CharField(max_length=20, default="private", choices=ACCESS_TYPES)
+    access = models.CharField(max_length=20, default="project", choices=ACCESS_TYPES)
 
     def get_k8s_values(self):
         k8s_values = super().get_k8s_values()
