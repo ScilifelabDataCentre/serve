6a8f95b4a31be95d21afdc378fd14c57fb46c3c6
alfredeen <johan.alfredeen@gmail.com>
2024-08-28 16:30:02 +0200

    Enabled the enableExtendedCollector setting in cypress installLogsCollector to output log messages from the before hook also to the terminal.

--- cypress/e2e/verify-test-setup.cy.js
+++ cypress/e2e/verify-test-setup.cy.js
@@ -2,22 +2,34 @@ describe("Simple tests to verify the test framework setup", () => {
 
   before(() => {
     // runs once before all tests in the block
-    cy.log("before() hook", `(TEST: ${Cypress.currentTest.title}, ${new Date().getTime()})`)
-})
+    cy.log("Start before() hook", `(TEST: ${Cypress.currentTest.title}, ${new Date().getTime()})`)
+    expect(Cypress.currentTest.titlePath[0]).to.eq('Simple tests to verify the test framework setup')
+    expect(Cypress.currentTest.title).to.be.a('string')
+    cy.log("End before() hook", `(TEST: ${Cypress.currentTest.title}, ${new Date().getTime()})`)
+  })
 
   beforeEach(() => {
     // runs before each test in the block
-    cy.log("beforeEach() hook", `(TEST: ${Cypress.currentTest.title}, ${new Date().getTime()})`)
+    cy.log("Start beforeEach() hook", `(TEST: ${Cypress.currentTest.title}, ${new Date().getTime()})`)
+    expect(Cypress.currentTest.titlePath[0]).to.eq('Simple tests to verify the test framework setup')
+    cy.log("End beforeEach() hook", `(TEST: ${Cypress.currentTest.title}, ${new Date().getTime()})`)
   })
 
   it("passes", () => {
   })
 
+  it("verify current test title", () => {
+    expect(Cypress.currentTest.titlePath[1]).to.eq('verify current test title')
+  })
+
   it("cypress can log to the terminal", () => {
+    expect(Cypress.currentTest.titlePath[1]).to.eq('cypress can log to the terminal')
     cy.log("Verify that this message is output to the terminal.", `(TEST: ${Cypress.currentTest.title}, ${new Date().getTime()})`)
   })
 
   it("can access and parse the test fixtures", () => {
+    expect(Cypress.currentTest.titlePath[1]).to.eq('can access and parse the test fixtures')
+
     cy.fixture('users.json').then(function (data) {
       cy.log(data.login_user.username, `(TEST: ${Cypress.currentTest.title}, ${new Date().getTime()})`)
       cy.log(data.contributor.username, `(TEST: ${Cypress.currentTest.title}, ${new Date().getTime()})`)
