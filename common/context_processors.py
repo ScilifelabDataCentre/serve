ea34f6eb23a13ecaa122c643f7b349bf848395c7
Hamza <hamzaimran08@gmail.com>
2024-09-15 20:05:04 +0200

    add exception catching to context processor

--- common/context_processors.py
+++ common/context_processors.py
@@ -1,6 +1,14 @@
+from studio.utils import get_logger
+
 from .models import MaintenanceMode
+
+logger = get_logger(__name__)
 
 
 def maintenance_mode(request):
-    data = MaintenanceMode.objects.all()
+    try:
+        data = MaintenanceMode.objects.all()
+    except Exception as e:
+        logger.debug("Error fetching maintenance mode data: %s", e)
+        data = []
     return {"maintenance_mode": data}
