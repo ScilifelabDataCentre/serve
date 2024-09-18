0774c207b58381e4ebb2c08ccceddb47e9765400
Nikita Churikov <nikita@chur.ru>
2024-09-18 14:11:57 +0200

    Added environment support for rstudio
    Signed-off-by: Nikita Churikov <nikita@chur.ru>

--- apps/forms/rstudio.py
+++ apps/forms/rstudio.py
@@ -1,5 +1,6 @@
 from crispy_forms.layout import HTML, Div, Field, Layout
 from django import forms
+from django.utils.safestring import mark_safe
 
 from apps.forms.base import AppBaseForm
 from apps.forms.field.common import SRVCommonDivField
@@ -10,6 +11,17 @@ __all__ = ["RStudioForm"]
 
 class RStudioForm(AppBaseForm):
     volume = forms.ModelMultipleChoiceField(queryset=VolumeInstance.objects.none(), required=False)
+    environment = forms.ModelChoiceField(queryset=None, required=True, empty_label=None)
+
+    def _setup_form_fields(self):
+        super()._setup_form_fields()
+        self.fields["environment"].label = "Environment"
+        self.fields["environment"].queryset = self.project.environment_set.filter(app__slug="rstudio")
+        self.fields["environment"].help_text = mark_safe(
+            "Select the environment to run the app in. "
+            "Read more about environments in the "
+            '<a href="https://serve.scilifelab.se/docs/notebooks/">documentation</a>.'
+        )
 
     def _setup_form_helper(self):
         super()._setup_form_helper()
@@ -18,6 +30,7 @@ class RStudioForm(AppBaseForm):
             Field("volume"),
             SRVCommonDivField("flavor"),
             SRVCommonDivField("access"),
+            SRVCommonDivField("environment"),
             css_class="card-body",
         )
 
@@ -25,4 +38,4 @@ class RStudioForm(AppBaseForm):
 
     class Meta:
         model = RStudioInstance
-        fields = ["name", "volume", "flavor", "access"]
+        fields = ["name", "volume", "flavor", "access", "environment"]
