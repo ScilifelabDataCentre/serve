5756ca8304ff6acd20370f68b34ae11a43246914
Mahbub Ul Alam <mahbub.ul.alam@scilifelab.se>
2024-10-24 15:18:50 +0200

    fixed typo in common/urls.py

--- common/urls.py
+++ common/urls.py
@@ -10,10 +10,6 @@ urlpatterns = [
     path("success/", views.RegistrationCompleteView.as_view(), name="success"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("verify/", views.VerifyView.as_view(), name="verify"),
-<<<<<<< HEAD
-    path("edit-profile/", login_required(views.EditProfileView.as_view()), name="edit-profile"),
-=======
     path("edit-profile/", views.EditProfileView.as_view(), name="edit-profile"),
     path("admin_profile_edit_disabled/", views.EditProfileView.as_view(), name="admin_profile_edit_disabled"),
->>>>>>> 0ed33936100075cb1b452129be1296397bbff6dd
 ]
