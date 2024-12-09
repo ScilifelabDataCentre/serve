0d2c73f4830eeaf3ee3a2aad22056b22758d15a0
akochari <arnold.kochari@scilifelab.se>
2024-12-09 16:41:59 +0100

    split unit tests into smaller tests

--- projects/tests/test_create_delete_environments.py
+++ projects/tests/test_create_delete_environments.py
@@ -12,96 +12,93 @@ test_user = {"username": "foo1", "email": "foo@test.com", "password": "bar"}
 test_superuser = {"username": "superuser", "email": "superuser@test.com", "password": "bar"}
 
 
-class EnvironmentTestCase(TestCase):
+class EnvironmentTestCaseRegularUser(TestCase):
     def setUp(self):
         user = User.objects.create_user(test_user["username"], test_user["email"], test_user["password"])
-        project = Project.objects.create_project(name="test-env", owner=user, description="")
+        self.project = Project.objects.create_project(name="test-env", owner=user, description="")
         User.objects.create_superuser(test_superuser["username"], test_superuser["email"], test_superuser["password"])
-        app = Apps.objects.create(name="Some App", slug="someapp")
-        Environment.objects.create(app=app, name="env-to-be-deleted", project=project)
+        self.app = Apps.objects.create(name="Some App", slug="someapp")
+        self.env_to_be_deleted = Environment.objects.create(app=self.app, name="env-to-be-deleted", project=self.project)
+        self.client.login(username=test_user["email"], password=test_user["password"])
 
-    def test_environment_creation_user(self):
+    def test_environment_creation_regular_user(self):
         """
         Test regular user not allowed to create environment
         """
-        self.client.login(username=test_user["email"], password=test_user["password"])
-
-        project = Project.objects.get(name="test-env")
-        app = Apps.objects.get(slug="someapp")
-
         n_envs_before = Environment.objects.count()
         response = self.client.post(
-            f"/projects/{project.slug}/createenvironment/",
+            f"/projects/{self.project.slug}/createenvironment/",
             {
                 "environment_name": "new-env-user",
                 "environment_repository": "n",
                 "environment_image": "n",
                 "environment_app": "n",
-                "app": app.pk,
+                "app": self.app.pk,
             },
         )
         self.assertEqual(response.status_code, 403)
         n_envs_after = Environment.objects.count()
-
         self.assertEqual(n_envs_before, n_envs_after)
 
+    def test_environment_deletion_regular_user(self):
         """
         Test regular user not allowed to delete environment
         """
-        env_to_be_deleted = Environment.objects.get(name="env-to-be-deleted")
         n_envs_before = Environment.objects.count()
         response = self.client.post(
-            f"/projects/{project.slug}/deleteenvironment/", {"environment_pk": env_to_be_deleted.pk}
+            f"/projects/{self.project.slug}/deleteenvironment/", {"environment_pk": self.env_to_be_deleted.pk}
         )
         self.assertEqual(response.status_code, 403)
         n_envs_after = Environment.objects.count()
         self.assertEqual(n_envs_before, n_envs_after)
 
-    def test_environment_creation_deletion_superuser(self):
+class EnvironmentTestCaseSuperUser(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(test_user["username"], test_user["email"], test_user["password"])
+        self.project = Project.objects.create_project(name="test-env", owner=self.user, description="")
+        User.objects.create_superuser(test_superuser["username"], test_superuser["email"], test_superuser["password"])
+        self.app = Apps.objects.create(name="Some App", slug="someapp")
+        self.env_to_be_deleted = Environment.objects.create(app=self.app, name="env-to-be-deleted", project=self.project)
+        self.client.login(username=test_superuser["email"], password=test_superuser["password"])
+
+    def test_environment_creation_superuser(self):
         """
         Test superuser is allowed to create environment
         """
-        self.client.login(username=test_superuser["email"], password=test_superuser["password"])
-        project = Project.objects.get(name="test-env")
-        app = Apps.objects.get(slug="someapp")
-
         n_envs_before = Environment.objects.count()
         response = self.client.post(
-            f"/projects/{project.slug}/createenvironment/",
+            f"/projects/{self.project.slug}/createenvironment/",
             {
                 "environment_name": "new-env-superuser",
                 "environment_repository": "n",
                 "environment_image": "n",
-                "environment_app": app.pk,
+                "environment_app": self.app.pk,
             },
         )
         self.assertEqual(response.status_code, 302)
         n_envs_after = Environment.objects.count()
-
         self.assertEqual(n_envs_before + 1, n_envs_after)
 
+    def test_environment_deletion_inuse_superuser(self):
         """
         Test it is not allowed to delete environment that is in use
         """
-        user = User.objects.get(email=test_superuser["email"])
-        env = Environment.objects.get(name="new-env-superuser")
-
+        env_cannot_be_deleted = Environment.objects.create(app=self.app, name="env-cannot-be-deleted", project=self.project)
         subdomain = Subdomain.objects.create(subdomain="test_internal")
         self.app_instance = JupyterInstance.objects.create(
             access="public",
-            owner=user,
+            owner=self.user,
             name="test_app_instance_env",
-            app=app,
-            project=project,
-            environment=env,
+            app=self.app,
+            project=self.project,
+            environment=env_cannot_be_deleted,
             subdomain=subdomain,
         )
-
-        can_env_be_deleted = can_model_instance_be_deleted("environment", env)
+        can_env_be_deleted = can_model_instance_be_deleted("environment", env_cannot_be_deleted)
         self.assertFalse(can_env_be_deleted)
 
         n_envs_before = Environment.objects.count()
-        response = self.client.post(f"/projects/{project.slug}/deleteenvironment/", {"environment_pk": env.pk})
+        response = self.client.post(f"/projects/{self.project.slug}/deleteenvironment/", {"environment_pk": env_cannot_be_deleted.pk})
         self.assertEqual(response.status_code, 302)
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
@@ -110,18 +107,15 @@ class EnvironmentTestCase(TestCase):
 
         self.assertEqual(n_envs_before, n_envs_after)
 
+    def test_environment_deletion_notinuse_superuser(self):
         """
         Test it is allowed to delete environment that is not in use
         """
-
-        env_to_be_deleted = Environment.objects.get(name="env-to-be-deleted")
-
-        can_env_be_deleted = can_model_instance_be_deleted("environment", env_to_be_deleted)
+        can_env_be_deleted = can_model_instance_be_deleted("environment", self.env_to_be_deleted)
         self.assertTrue(can_env_be_deleted)
-
-        n_env_before = Environment.objects.count()
+        n_envs_before = Environment.objects.count()
         response = self.client.post(
-            f"/projects/{project.slug}/deleteenvironment/", {"environment_pk": env_to_be_deleted.pk}
+            f"/projects/{self.project.slug}/deleteenvironment/", {"environment_pk": self.env_to_be_deleted.pk}
         )
         self.assertEqual(response.status_code, 302)
         n_envs_after = Environment.objects.count()
--- projects/tests/test_create_delete_flavors.py
+++ projects/tests/test_create_delete_flavors.py
@@ -12,24 +12,20 @@ test_user = {"username": "foo1", "email": "foo@test.com", "password": "bar"}
 test_superuser = {"username": "superuser", "email": "superuser@test.com", "password": "bar"}
 
 
-class FlavorTestCase(TestCase):
+class FlavorTestCaseRegularUser(TestCase):
     def setUp(self):
         user = User.objects.create_user(test_user["username"], test_user["email"], test_user["password"])
-        project = Project.objects.create_project(name="test-flavor", owner=user, description="")
-        User.objects.create_superuser(test_superuser["username"], test_superuser["email"], test_superuser["password"])
-        Flavor.objects.create(name="flavor-to-be-deleted", project=project)
+        self.project = Project.objects.create_project(name="test-flavor", owner=user, description="")
+        self.flavor_to_be_deleted = Flavor.objects.create(name="flavor-to-be-deleted", project=self.project)
+        self.client.login(username=test_user["email"], password=test_user["password"])
 
-    def test_flavor_creation_user(self):
+    def test_flavor_creation_regular_user(self):
         """
         Test regular user not allowed to create flavor
         """
-        self.client.login(username=test_user["email"], password=test_user["password"])
-
-        project = Project.objects.get(name="test-flavor")
-
         n_flavors_before = Flavor.objects.count()
         response = self.client.post(
-            f"/projects/{project.slug}/createflavor/",
+            f"/projects/{self.project.slug}/createflavor/",
             {
                 "flavor_name": "new-flavor-user",
                 "cpu_req": "n",
@@ -42,28 +38,33 @@ class FlavorTestCase(TestCase):
         )
         self.assertEqual(response.status_code, 403)
         n_flavors_after = Flavor.objects.count()
-
         self.assertEqual(n_flavors_before, n_flavors_after)
 
+    def test_flavor_deletion_regular_user(self):
         """
         Test regular user not allowed to delete flavor
         """
-        flavor_to_be_deleted = Flavor.objects.get(name="flavor-to-be-deleted")
         n_flavors_before = Flavor.objects.count()
-        response = self.client.post(f"/projects/{project.slug}/deleteflavor/", {"flavor_pk": flavor_to_be_deleted.pk})
+        response = self.client.post(f"/projects/{self.project.slug}/deleteflavor/", {"flavor_pk": self.flavor_to_be_deleted.pk})
         self.assertEqual(response.status_code, 403)
         n_flavors_after = Flavor.objects.count()
         self.assertEqual(n_flavors_before, n_flavors_after)
 
-    def test_flavor_creation_deletion_superuser(self):
+class FlavorTestCaseSuperUser(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(test_user["username"], test_user["email"], test_user["password"])
+        self.project = Project.objects.create_project(name="test-flavor", owner=self.user, description="")
+        User.objects.create_superuser(test_superuser["username"], test_superuser["email"], test_superuser["password"])
+        self.flavor_to_be_deleted = Flavor.objects.create(name="flavor-to-be-deleted", project=self.project)
+        self.client.login(username=test_superuser["email"], password=test_superuser["password"])
+
+    def test_flavor_creation_superuser(self):
         """
         Test superuser is allowed to create flavor
         """
-        self.client.login(username=test_superuser["email"], password=test_superuser["password"])
-        project = Project.objects.get(name="test-flavor")
         n_flavors_before = Flavor.objects.count()
         response = self.client.post(
-            f"/projects/{project.slug}/createflavor/",
+            f"/projects/{self.project.slug}/createflavor/",
             {
                 "flavor_name": "new-flavor-superuser",
                 "cpu_req": "n",
@@ -76,33 +77,32 @@ class FlavorTestCase(TestCase):
         )
         self.assertEqual(response.status_code, 302)
         n_flavors_after = Flavor.objects.count()
-
         self.assertEqual(n_flavors_before + 1, n_flavors_after)
 
+    def test_flavor_deletion_inuse_superuser(self):
         """
         Test it is not allowed to delete flavor that is in use
         """
-        user = User.objects.get(email=test_superuser["email"])
-        flavor = Flavor.objects.get(name="new-flavor-superuser")
+        flavor_cannot_be_deleted = Flavor.objects.create(name="flavor-cannot-be-deleted", project=self.project)
 
         app = Apps.objects.create(name="Some App", slug="customapp")
         subdomain = Subdomain.objects.create(subdomain="test_internal")
         self.app_instance = CustomAppInstance.objects.create(
             access="public",
-            owner=user,
+            owner=self.user,
             name="test_app_instance_flavor",
             description="some app description",
             app=app,
-            project=project,
-            flavor=flavor,
+            project=self.project,
+            flavor=flavor_cannot_be_deleted,
             subdomain=subdomain,
         )
 
-        can_flavor_be_deleted = can_model_instance_be_deleted("flavor", flavor)
+        can_flavor_be_deleted = can_model_instance_be_deleted("flavor", flavor_cannot_be_deleted)
         self.assertFalse(can_flavor_be_deleted)
 
         n_flavors_before = Flavor.objects.count()
-        response = self.client.post(f"/projects/{project.slug}/deleteflavor/", {"flavor_pk": flavor.pk})
+        response = self.client.post(f"/projects/{self.project.slug}/deleteflavor/", {"flavor_pk": flavor_cannot_be_deleted.pk})
         self.assertEqual(response.status_code, 302)
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
@@ -111,17 +111,15 @@ class FlavorTestCase(TestCase):
 
         self.assertEqual(n_flavors_before, n_flavors_after)
 
+    def test_flavor_deletion_notinuse_superuser(self):
         """
         Test it is allowed to delete flavor that is not in use
         """
-
-        flavor_to_be_deleted = Flavor.objects.get(name="flavor-to-be-deleted")
-
-        can_flavor_be_deleted = can_model_instance_be_deleted("flavor", flavor_to_be_deleted)
+        can_flavor_be_deleted = can_model_instance_be_deleted("flavor", self.flavor_to_be_deleted)
         self.assertTrue(can_flavor_be_deleted)
 
         n_flavors_before = Flavor.objects.count()
-        response = self.client.post(f"/projects/{project.slug}/deleteflavor/", {"flavor_pk": flavor_to_be_deleted.pk})
+        response = self.client.post(f"/projects/{self.project.slug}/deleteflavor/", {"flavor_pk": self.flavor_to_be_deleted.pk})
         self.assertEqual(response.status_code, 302)
         n_flavors_after = Flavor.objects.count()
 
