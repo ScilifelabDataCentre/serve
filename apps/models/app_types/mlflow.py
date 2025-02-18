cace4cef6ad7883247b6d211a172fcc19d7cef17
Nikita Churikov <8545082+churnikov@users.noreply.github.com>
2025-02-18 17:53:17 +0100

    SS-1288 mlflow default resources (#280)
    Signed-off-by: Nikita Churikov <nikita@chur.ru>

--- apps/models/app_types/mlflow.py
+++ apps/models/app_types/mlflow.py
@@ -21,7 +21,17 @@ class MLFlowInstance(BaseAppInstance):
                 "ingressClassName": "nginx",
                 "hostname": self.url.split("://")[1] if self.url is not None else self.url,
             },
+            "resources": {
+                "requests": {"cpu": "1", "memory": "512Mi", "ephemeral-storage": "512Mi"},
+                "limits": {"cpu": "2", "memory": "1Gi", "ephemeral-storage": "1Gi"},
+            },
             "pdb": {"create": False},
+        }
+        k8s_values["run"] = {
+            "resources": {
+                "requests": {"cpu": "1", "memory": "512Mi", "ephemeral-storage": "512Mi"},
+                "limits": {"cpu": "2", "memory": "1Gi", "ephemeral-storage": "1Gi"},
+            }
         }
         k8s_values["minio"] = {"pdb": {"create": False}}
         k8s_values["postgresql"] = {
