fe55261c51bb2492c42c76798467c66e81f6278f
Nikita Churikov <8545082+churnikov@users.noreply.github.com>
2025-04-09 13:06:41 +0200

    Remove subdomain field from the MLFlow form (#318)
    Signed-off-by: Nikita Churikov <nikita@chur.ru>

--- apps/forms/mlflow.py
+++ apps/forms/mlflow.py
@@ -1,4 +1,4 @@
-from crispy_forms.layout import Div, Field, Layout
+from crispy_forms.layout import HTML, Div, Field, Layout
 
 from apps.forms.base import BaseForm
 from apps.models import MLFlowInstance
@@ -12,8 +12,21 @@ class MLFlowAppForm(BaseForm):
     def _setup_form_helper(self):
         super()._setup_form_helper()
         body = Div(
+            HTML(
+                """
+                <div class="alert alert-info" role="alert">
+                <p>⚠️ Experimental Feature Notice</p>
+                <p>This feature is currently in an experimental phase. Please use it with caution as
+                stability and data integrity are not guaranteed. The feature could change at any time.</p>
+                </div>
+                <p>You are about to create a MLFlow app on SciLifeLab Serve.
+                You can use it to track your machine learning experiments.
+                You can start using it right away after it's created and we suggest you to start with
+                reading the <a href="https://serve.scilifelab.se/docs/mlflow/">Serve User Guide for MLFlow</a> and
+                <a href="https://mlflow.org/docs/latest/index.html">MLFlow Documentation</a>.</p>
+                """
+            ),
             Field("name", placeholder="Name your app"),
-            Field("subdomain", placeholder="Enter a subdomain or leave blank for a random one"),
             css_class="card-body",
         )
 
