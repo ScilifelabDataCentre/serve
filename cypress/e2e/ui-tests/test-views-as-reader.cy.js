15e224cb0809b61194c3820b702f6f5a1b95ab7b
Mahbub Ul Alam <mahbub.ul.alam@scilifelab.se>
2025-05-05 16:58:19 +0200

    SS-1219 Django views to manage e2e_test data (#320)
    Source: https://scilifelab.atlassian.net/browse/SS-1219
    
    Creating Django views to manage e2e_test data.
    
    Data can be managed (creation and deletion of users, projects, and apps) via Django view endpoints.
    
    Integrated with Cypress e2e tests.
    
    The Cypress test configuration is customizable, making it easy to revert to the previous script-based seeding method if needed.
    
    Two levels of security are ensured:
    
    (a) Accessible only when DEBUG mode is True
    (b) CSRF token required
    
     Unit test cases have also been written.

--- cypress/e2e/ui-tests/test-views-as-reader.cy.js
+++ cypress/e2e/ui-tests/test-views-as-reader.cy.js
@@ -4,27 +4,38 @@ describe("Test views as authenticated user", () => {
   // user: e2e_tests_login_tester
 
   let users
+  let TEST_USER_DATA
 
   before(() => {
     cy.logf("Begin before() hook", Cypress.currentTest)
-    // do db reset if needed
-    if (Cypress.env('do_reset_db') === true) {
-        cy.logf("Resetting db state. Running db-reset.sh", Cypress.currentTest);
-        cy.exec("./cypress/e2e/db-reset.sh");
-        cy.wait(Cypress.env('wait_db_reset'));
+
+    if (Cypress.env('manage_test_data_via_django_endpoint_views') === true) {
+      cy.log("Populating test data via Django endpoint");
+      cy.fixture('users.json').then(function (data) {
+        TEST_USER_DATA = data.reader_user;
+        cy.populateTestUser(TEST_USER_DATA);
+      })
     }
     else {
-        cy.logf("Skipping resetting the db state.", Cypress.currentTest);
+        // do db reset if needed
+        if (Cypress.env('do_reset_db') === true) {
+          cy.logf("Resetting db state. Running db-reset.sh", Cypress.currentTest);
+          cy.exec("./cypress/e2e/db-reset.sh");
+          cy.wait(Cypress.env('wait_db_reset'));
+        }
+        else {
+          cy.logf("Skipping resetting the db state.", Cypress.currentTest);
+        }
+      // seed the db with a user
+      cy.visit("/")
+      cy.logf("Running seed_reader_user.py", Cypress.currentTest)
+      cy.exec("./cypress/e2e/db-seed-reader-user.sh")
     }
-    // seed the db with a user
-    cy.visit("/")
-    cy.logf("Running seed_reader_user.py", Cypress.currentTest)
-    cy.exec("./cypress/e2e/db-seed-reader-user.sh")
+
     // log in
     cy.logf("Logging in", Cypress.currentTest)
     cy.fixture('users.json').then(function (data) {
         users = data
-
         cy.loginViaApi(users.reader_user.email, users.reader_user.password)
     })
 
@@ -45,5 +56,15 @@ describe("Test views as authenticated user", () => {
 
     cy.get('h3').should('have.text', 'Login required')
   })
+
+  after(() => {
+
+    if (Cypress.env('manage_test_data_via_django_endpoint_views') === true) {
+      cy.log("Cleaning up test data via Django endpoint");
+      cy.cleanupTestUser(TEST_USER_DATA);
+    }
+
+    cy.logf("End after() hook", Cypress.currentTest)
+})
 
 })
