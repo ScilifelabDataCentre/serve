8e367ae210fe1474b4ac39f750c809010b0e07a0
Mahbub Ul Alam <mahbub.ul.alam@scilifelab.se>
2025-05-12 12:38:29 +0200

    delete projects before deleting user

--- common/management/commands/remove_locust_users.py
+++ common/management/commands/remove_locust_users.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 
+from common.management.manage_test_data import TestDataManager
+
 User = get_user_model()
 
 
@@ -12,6 +14,12 @@ class Command(BaseCommand):
 
         # Find users with the specified email identifier
         users_to_delete = User.objects.filter(email__contains=identifier)
+
+        # Delete all projects first (if any)
+        for user in users_to_delete:
+            manager = TestDataManager(user_data={"email": user.email})
+            manager.delete_all_projects()
+            self.stdout.write(self.style.SUCCESS(f'Successfully deleted all projects (if any) for user "{user.email}"'))
 
         # Delete the users
         user_count = users_to_delete.count()
