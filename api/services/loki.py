78714f21dacac23fe2dee57db05a0791966585b5
j-awada <jana.awada@scilifelab.uu.se>
2025-06-24 10:28:25 +0200

    update loki query

--- api/services/loki.py
+++ api/services/loki.py
@@ -14,7 +14,7 @@ def query_unique_ip_count(namespace: str = "loki-stack", app_subdomain: str = ""
     Discover the Loki reader pod in the given namespace and query it for the count of unique ingress client IPs.
     The app_subdomain parameter is used to filter logs for the specific app.
     Returns the count as an integer.
-    """
+    
     if not app_subdomain:
         logger.error("app_subdomain must be provided")
         raise ValueError("app_subdomain must be provided")
@@ -22,18 +22,18 @@ def query_unique_ip_count(namespace: str = "loki-stack", app_subdomain: str = ""
     if not pod_name:
         logger.error(f"Could not find Loki reader pod in namespace {namespace}")
         raise ValueError(f"Could not find Loki reader pod in namespace {namespace}")
+    """
 
-    loki_reader_host = f"http://{pod_name}.loki-stack.{namespace}.svc.cluster.local:3100"
+    loki_reader_host = f"http://loki-read-headless.loki-stack.svc.cluster.local:3100"
     endpoint = f"{loki_reader_host}/loki/api/v1/query_range"
 
     end_time = f"{int(time.time() * 1_000_000_000)}"
     start_time = f"{int((time.time() - (29 * 24 * 60 * 60)) * 1_000_000_000)}"
 
-    query = rf"""
-{{container="rke2-ingress-nginx-controller"}} |= "{app_subdomain}" \
-| regexp "(?P<client_ip>\\b\\d{{1,3}}\\.\\d{{1,3}}\\.\\d{{1,3}}\\.\\d{{1,3}}\\b)"\
-| line_format "{{{{.client_ip}}}}"
-"""
+    query = r'{container="rke2-ingress-nginx-controller"} |= "' + app_subdomain + '" ' + \
+        r'| regexp "(?P<client_ip>\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b)" ' + \
+        r'| line_format "{{.client_ip}}"'
+
     params = {
         "query": query,
         "start": start_time,
