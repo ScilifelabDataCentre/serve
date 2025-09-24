232239674b5318bceceff6b3004afadbe40be385
j-awada <jana.awada@scilifelab.uu.se>
2025-09-24 09:32:20 +0200

    update redis info, variable and db

--- studio/settings.py
+++ studio/settings.py
@@ -577,7 +577,7 @@ LOKI_READER_ENDPOINT = "http://loki-read-headless.loki-stack.svc.cluster.local:3
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}",  # https://redis.io/docs/latest/commands/select/
+        "LOCATION": f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/1",  # https://redis.io/docs/latest/commands/select/
         "KEY_PREFIX": "serve",
         "TIMEOUT": 7200,  # 2 hours default but will be overriden
     }
