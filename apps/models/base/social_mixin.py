aad739ef74956187e5f32edf81c2003a27e247e7
arnoldko <arnold.kochari@scilifelab.uu.se>
2025-12-03 09:11:58 +0100

    add task to send reminders

--- apps/models/base/social_mixin.py
+++ apps/models/base/social_mixin.py
@@ -5,6 +5,7 @@ from tagulous.models import TagField
 class SocialMixin(models.Model):
     tags = TagField(blank=True, help_text="Add keywords to help categorize your app", force_lowercase=True)
     note_on_linkonly_privacy = models.TextField(blank=True, null=True, default="")
+    reminder_date_linkonly_privacy = models.DateField(null=True, blank=True)
     collections = models.ManyToManyField("portal.Collection", blank=True, related_name="%(class)s")
     source_code_url = models.URLField(blank=True, null=True)
     description = models.TextField(default="")
