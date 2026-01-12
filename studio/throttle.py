f2bdc4560da5e49329d73c3be80ffd7a5274af22
j-awada <jana.awada@scilifelab.uu.se>
2026-01-12 15:12:03 +0100

    fix typos

--- studio/throttle.py
+++ studio/throttle.py
@@ -15,9 +15,9 @@ class WhitelistThrottleFilter(UserRateThrottle):
     rate = getattr(settings, "AUTH_RATE_LIMIT_VALUE", "10/minute")
 
     def allow_request(self, request: HttpRequest, view: APIView) -> Any:
-        incomming_ip = self.get_ident(request)
-        whitelist_range = getattr(settings, "RATE_LIMIT_WHITELIST", [])
+        incoming_ip = self.get_ident(request)
+        whitelist_range = getattr(settings, "AUTH_RATE_LIMIT_WHITELIST", [])
         for network in whitelist_range:
-            if ip_address(incomming_ip) in ip_network(network):
+            if ip_address(incoming_ip) in ip_network(network):
                 return True
         return super().allow_request(request, view)
