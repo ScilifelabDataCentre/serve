53e5810eb75497844e32405f8f250dcda191d0c2
j-awada <jana.awada@scilifelab.uu.se>
2026-01-12 12:31:02 +0100

    lint code and adjust rate limit value

--- studio/throttle.py
+++ studio/throttle.py
@@ -6,11 +6,13 @@ from django.http import HttpRequest
 from rest_framework.throttling import UserRateThrottle
 from rest_framework.views import APIView
 
+
 class WhitelistThrottleFilter(UserRateThrottle):
     """
     Custom throttle filter that whitelists certain IP ranges
     """
-    rate = "100/minute"
+
+    rate = "10/minute"
 
     def allow_request(self, request: HttpRequest, view: APIView) -> Any:
         incomming_ip = self.get_ident(request)
