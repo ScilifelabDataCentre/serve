eb033c154a5b9a584e42c1f586e52abcafa4e784
Mahbub Ul Alam <mahbub.ul.alam@scilifelab.se>
2026-01-14 16:02:16 +0100

    fix old profile migration

--- common/management/manage_test_data.py
+++ common/management/manage_test_data.py
@@ -12,6 +12,10 @@ from projects.models import (
 )
 from projects.tasks import create_resources_from_template
 
+from studio.utils import get_logger
+
+logger = get_logger(__name__)
+
 
 class TestDataManager:
     def __init__(self, user_data=None, project_data=None, app_data=None):
@@ -36,12 +40,20 @@ class TestDataManager:
         user.is_active = True
         user.save()
 
-        # Profile creation
-        if all(field in self.user_data for field in ("department", "affiliation")):
+        # Profile creation, # TODO fix e2e tests accordingly, all will break otherwise
+        if any(field in self.user_data for field in ("affiliation", "organization")):
             user_profile = UserProfile.objects.create_user_profile(user)
+        
+        if "department" in self.user_data:
             user_profile.department = self.user_data["department"]
+            
+        if "organization" in self.user_data:
+            user_profile.organization = self.user_data["organization"]
+        
+        if "affiliation" in self.user_data:
             user_profile.affiliation = self.user_data["affiliation"]
-            user_profile.save()
+            
+        user_profile.save()
 
         return user
 
@@ -64,7 +76,7 @@ class TestDataManager:
 
         user_to_delete = User.objects.filter(email__exact=self.user_data["email"])
 
-        if all(field in self.user_data for field in ("department", "affiliation")):
+        if any(field in self.user_data for field in ("department", "affiliation", "organization")):
             UserProfile.objects.filter(user__in=user_to_delete).delete()
 
         deleted_count, _ = user_to_delete.delete()
