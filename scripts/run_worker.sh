c3dca484e74ea21e4e4fdfcf3e42682acce5227a
Hamza <hamza.saeed@scilifelab.uu.se>
2026-03-03 20:58:42 +0100

    Hotfix/SS-1807 patch inconsistent deployment (#478)

--- scripts/run_worker.sh
+++ scripts/run_worker.sh
@@ -4,8 +4,11 @@ set -e
 # Giving time to studio container to run DB migrations
 sleep 25
 
+CELERY_LOG_LEVEL=${CELERY_LOG_LEVEL:-info}
+echo "Starting celery worker at log level: ${CELERY_LOG_LEVEL}"
+
 if $DEBUG ; then
-    watchmedo auto-restart -R --patterns="*.py" -- celery -A studio worker -l info --scheduler django
+    watchmedo auto-restart -R --patterns="*.py" -- celery -A studio worker -l "${CELERY_LOG_LEVEL}" --scheduler django
 else
-    celery -A studio worker -l info --scheduler django
+    celery -A studio worker -l "${CELERY_LOG_LEVEL}" --scheduler django
 fi
