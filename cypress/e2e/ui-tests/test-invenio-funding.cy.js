b583b208a8ba05a6359bc69790e45a7b15dd4299
Hamza <hamzaimran08@gmail.com>
2026-03-05 03:08:35 +0100

    update invenio e2e test

--- cypress/e2e/ui-tests/test-invenio-funding.cy.js
+++ cypress/e2e/ui-tests/test-invenio-funding.cy.js
@@ -122,7 +122,7 @@ if (Cypress.env("create_resources") === true) {
             cy.intercept(
                 {
                     method: "GET",
-                    pathname: /\/api\/invenio\/funders\/?$/,
+                    pathname: "/api/invenio/funders/",
                     query: {
                         q: /.+/
                     }
