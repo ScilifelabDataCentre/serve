fb58222425b081b4f7aa5da995c7ee51dbd8dc45
Jana Awada <jana.awada@scilifelab.uu.se>
2026-03-20 11:37:16 +0100

    unit test for public app deletion

--- apps/tests/test_delete_app_view.py
+++ apps/tests/test_delete_app_view.py
@@ -29,9 +29,9 @@ class DeleteAppViewTestCase(TestCase):
         k8s_user_app_status = K8sUserAppStatus.objects.create()
 
         self.app_instance = JupyterInstance.objects.create(
-            access="public",
+            access="private",
             owner=self.user,
-            name="test_app_instance_public",
+            name="test_app_instance",
             app=self.app,
             project=self.project,
             subdomain=subdomain,
@@ -77,9 +77,41 @@ class DeleteAppViewTestCase(TestCase):
 
             self.assertEqual(response.status_code, 302)
 
-            self.app_instance = JupyterInstance.objects.get(name="test_app_instance_public")
+            self.app_instance = JupyterInstance.objects.get(name="test_app_instance")
 
             self.assertEqual("private", self.app_instance.access)
             self.assertIsNotNone(self.app_instance.deleted_on)
 
             mock_task.assert_called_once()
+
+    def test_public_app_cannot_be_deleted(self):
+        """Test that public apps cannot be deleted regardless of user_can_delete setting"""
+        c = Client()
+
+        response = c.post("/accounts/login/", {"username": test_user["email"], "password": test_user["password"]})
+        self.assertEqual(response.status_code, 302)
+
+        # Create a public app instance for this test
+        subdomain = Subdomain.objects.create(subdomain="test_public")
+        k8s_user_app_status = K8sUserAppStatus.objects.create()
+        
+        public_app_instance = JupyterInstance.objects.create(
+            access="public",
+            owner=self.user,
+            name="test_public_app_instance",
+            app=self.app,
+            project=self.project,
+            subdomain=subdomain,
+            k8s_user_app_status=k8s_user_app_status,
+        )
+
+        # Even with user_can_delete=True, public apps should not be deletable
+        self.app.user_can_delete = True
+        self.app.save()
+
+        url = f"/projects/{self.project.slug}/apps/delete/" + f"{public_app_instance.app.slug}/{public_app_instance.id}"
+
+        response = c.get(url)
+
+        # Should return 403 Forbidden
+        self.assertEqual(response.status_code, 403)
