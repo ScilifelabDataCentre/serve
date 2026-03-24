bb4a7c5ddfb6e1aab2ef1350c833eafe0c29cf5f
Hamza <hamzaimran08@gmail.com>
2026-03-24 14:55:15 +0100

    update image validation app types

--- apps/background_tasks/tasks/validation.py
+++ apps/background_tasks/tasks/validation.py
@@ -15,7 +15,10 @@ logger = get_logger(__name__)
 
 
 @TASK_REGISTRY.register(
-    name="validate_docker_image", is_critical=True, execution_order=1, app_types=["customapp", "jupyter", "rstudio"]
+    name="validate_docker_image",
+    is_critical=True,
+    execution_order=1,
+    app_types=["customapp", "dashapp", "jupyter-lab", "rstudio", "shinyproxyapp", "shinyapp", "gradio", "streamlit"],
 )
 class DockerImageValidator(BaseBackgroundTask):
     """
