fffc3c654ce2773a606b5f18b011697daf974b32
arnoldko <arnold.kochari@scilifelab.uu.se>
2026-04-28 17:01:12 +0100

    hide invenio fields when selected permission not public

--- apps/forms/shiny.py
+++ apps/forms/shiny.py
@@ -90,8 +90,8 @@ class ShinyForm(StorageMixin, ContainerImageMixin, KeywordTagsValidationMixin, C
             SRVCommonDivField("name", required=True),
             SRVCommonDivField("description", rows=4, required=True),
             SRVCommonDivField("invenio_tags", template="apps/invenio_tags_field.html"),
-            self.get_creators_field_layout(),
             SRVCommonDivField("access"),
+            self.get_creators_field_layout(),
             SRVCommonDivField(
                 "note_on_linkonly_privacy",
                 rows=1,
