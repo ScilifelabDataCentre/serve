f76426aa9faf770b32fea0401372af33dd81c596
Hamza <hamza.saeed@scilifelab.uu.se>
2026-06-12 16:00:40 +0200

    fix login signup filter

--- common/templatetags/is_login_signup_disabled.py
+++ common/templatetags/is_login_signup_disabled.py
@@ -4,5 +4,6 @@ register = template.Library()
 
 
 @register.filter(name="is_login_signup_disabled")
-def is_login_signup_disabled(queryset):
-    return queryset.filter(login_and_signup_disabled=True).exists()
+def is_login_signup_disabled(items):
+    # `items` may be a QuerySet or a cached list (see common.context_processors)
+    return any(obj.login_and_signup_disabled for obj in items)
